1780bd348f5563446c372b2f190827a9c0b04d23
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2023-01-16 18:18:40 -0500

    added tabs

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ let pointer;
 
 oscillator.connect(gainNode).connect(audioContext.destination); resetVars();
 
+
+function openCity(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("defaultOpen").click();
+
+
+
 function startGame() {
     myGameArea.start();
     pointer = new component(5, 128 * 5, "blue", myGameArea.canvas.width/2, 0, 
